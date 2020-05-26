61a046749d57e7a42c6f9c191394eafb5c172c69
Jayesh Dhandha <jayudhandha@gmail.com>
2020-05-26 08:28:29 +0530

    Add files via upload

--- ListExample.py
+++ ListExample.py
@@ -1,5 +1,86 @@
-listA = ["PHP", "Exercise", "TEST"]
+# List
+# Tuple
+# Set
+# Dictionary
 
-for i in listA:
-	print(len(i))
+listA = ["Jayesh", 25, 8000, 10.5, "Vadodara"]
 
+listB = [1,2,3]
+
+listB.remove(3)
+
+# print(listA + listB)
+
+# print(listA)
+
+print(listB)
+
+# listA.extend(listB)
+
+# listA[2] = 10000
+
+# popedElement = listA.pop(1)
+
+# print(popedElement)
+
+# print(listA)
+
+# print(len(listA))
+
+# slicing, append/add, remove, length , replace
+# concate, find some element in list, iterate
+
+# print(listA[1:4])
+
+# print(listA[-1])
+
+# print(listA[2:])
+
+# for i in listA:
+#     print(i)
+
+# if 8000 in listA:
+#     print("Value exist")
+# else:
+#     print("Value doesn't exist")
+
+# listA.append("New Value")
+
+# print(listA)
+
+# listA.remove(8000)
+
+# print(listA)
+
+# listNo = [1,2,3,4,5,6,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,6,6,6,6,7,7,7]
+
+##### Nikita #####
+
+# listA = [1,2,2,2,2,2,2,3,3,3,3,3,3,4,5,6,7,8,8,8]
+# listB = []
+# for i in listA:
+#     if i not in listB:
+#         listB.append(i)
+# print(listB)
+
+
+# ##### Rahi #####
+
+# # listb =[]
+# # for x in listNo:
+# #     if x not in listb:
+# #         listb.append(x)
+
+# # print(listb)
+
+
+# ##### Elia #####
+
+# listNo = [1,2,3,4,5,6,2,2,2,2,2,2,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,5,5,5,5,5,6,6,6,6,7,7,7]
+# listoutput = []
+
+# for i in listNo:
+#     if i not in listoutput:
+#         listoutput.append(i)
+
+# print(listoutput)
---
+++ TupleExample.py
@@ -0,0 +1,24 @@
+# tuple
+
+# listB = [1,2,3]
+
+# tupleA = (1,2,3,4,5)
+
+# Tuples are immutable (unmodifiable collection)
+
+# tupleA.append(10)
+
+# tupleA.remove(10)
+
+# print(tupleA[2:])
+
+# for i in tupleA:
+#     print(i)
+
+# list of tuples
+
+# Employees = [(101, "Ayush", 22), (102, "john", 29), (103, "james", 45), (104, "Ben", 34)]  
+
+# Employees[0] = (500, "test", 30)
+
+# print(Employees)
